c67cb41a00949ef722dc54f93775999b3a1de3c5
vijay <vijayghaghada@yahoo.in>
2021-10-13 20:04:01 +0200

    test pipiline

--- lambda_function.py
+++ lambda_function.py
@@ -9,7 +9,7 @@ from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
        rating=random.randint(0, 11)
-       text='You are good'
+       text='You are good at'
        body=json.loads(event['body'])
        text=body['text']
        text=text.replace(',','').replace('.','').replace('!','')
